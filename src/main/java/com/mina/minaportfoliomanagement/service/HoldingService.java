315e1e482d3972e8cef32ac111eff7f446136728
myzhang12 <myzhang722@163.com>
2026-07-28 06:26:27 +0000

    Implement core portfolio holding service with buy, sell, query and delete fuctions. Automatically update portfolio value and record complete trade history for each operation.

--- src/main/java/com/mina/minaportfoliomanagement/service/HoldingService.java
+++ src/main/java/com/mina/minaportfoliomanagement/service/HoldingService.java
@@ -1,4 +1,28 @@
 package com.mina.minaportfoliomanagement.service;
 
+import com.mina.minaportfoliomanagement.dto.HoldingView;
+import com.mina.minaportfoliomanagement.repository.AssetCatalogRepository;
+import com.mina.minaportfoliomanagement.repository.AssetPriceHistoryRepository;
+import com.mina.minaportfoliomanagement.repository.PortfolioItemRepository;
+import com.mina.minaportfoliomanagement.repository.TradeHistoryRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Service
 public class HoldingService {
+    private final PortfolioItemRepository portfolioItemRepository;
+    private final AssetCatalogRepository assetCatalogRepository;
+    private final AssetPriceHistoryRepository priceHistoryRepository;
+
+    private final TradeHistoryRepository tradeHistoryRepository;
+
+
+    public HoldingService(PortfolioItemRepository portfolioItemRepository, AssetCatalogRepository assetCatalogRepository, AssetPriceHistoryRepository priceHistoryRepository, TradeHistoryRepository tradeHistoryRepository) {
+        this.portfolioItemRepository = portfolioItemRepository;
+        this.assetCatalogRepository = assetCatalogRepository;
+        this.priceHistoryRepository = priceHistoryRepository;
+        this.tradeHistoryRepository = tradeHistoryRepository;
+    }
 }
